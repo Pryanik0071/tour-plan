35ccede14c0b0c45a58aa8ba8d42f02119e5e8ce
Dmitry M <di.mesler.2010@yandex.ru>
2021-07-21 23:05:20 +0300

    Add: change Yandex constructor map for Yandex API maps
    Signed-off-by: Dmitry M <di.mesler.2010@yandex.ru>

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,3 +1,4 @@
+// Слайдер
 const swiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
@@ -13,3 +14,27 @@ const swiper = new Swiper(".swiper-container", {
     enabled: true,
   },
 });
+
+// Карта
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+  var myMap = new ymaps.Map("map", {
+    center: [7.890712, 98.294725],
+    zoom: 17,
+  });
+  // Создание геообъекта с типом точка (метка).
+  var myPlacemark = new ymaps.Placemark(
+    [7.890712, 98.294725],
+    {
+      balloonContentHeader: "DoubleTree by Hilton Phuket Banthai Resort",
+    },
+    {
+      preset: "islands#redHotelIcon",
+    }
+  );
+
+  // Размещение геообъекта на карте.
+  myMap.geoObjects.add(myPlacemark);
+}
