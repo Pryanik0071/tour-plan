fc84904a851f18d2d76a0ebd22a4a86611f170e2
Dmitry M <di.mesler.2010@yandex.ru>
2021-07-28 19:28:18 +0300

    Add: mobile menu + adaptive
    Signed-off-by: Dmitry M <di.mesler.2010@yandex.ru>

--- templates/js/main.js
+++ templates/js/main.js
@@ -59,3 +59,9 @@ const swiperReviews = new Swiper(".reviews-container", {
 
 // Parallax
 $(".newsletter").parallax({ imageSrc: "templates/img/newsletter-bg.jpg" });
+
+// Кнопка в шапке
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  document.querySelector(".nav-menu").classList.toggle("nav-menu--visible");
+});
